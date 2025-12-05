fc036ae4911b60426113dc287cccc68821ff53ee
naveenapyd <naveenapyd@gmail.com>
2025-12-05 11:10:32 +0530

    Improve day3 puzzle1 (iteration4) and add readme

---
+++ day3/README.md
@@ -0,0 +1,20 @@
+# Day 3: Lobby
+[Day 3 puzzle in Advent of code 2025](https://adventofcode.com/2025/day/3)  
+
+[Part 1 Solution](puzzle1.py)  
+[Part 2 Solution](puzzle2.py)  
+
+```
+Note: Add your own input in this folder with filename: puzzle_input.txt
+```
+
+## Time to run the code
+
+### Part 1
+`Iteration 1`: 0.479 sec  
+`Iteration 2`: 0.264 sec  
+`Iteration 3`: 0.043 sec  
+`Iteration 4`: 0.012 sec  
+
+### Part 2
+`Iteration 1`: 0.023 sec
--- day3/puzzle1.py
+++ day3/puzzle1.py
@@ -18,28 +18,28 @@ def list_of_input():
   return largest_possible_joltage(entries)
 
 def largest_possible_joltage(entries):
-  ''' Each entry is checked for the largest possible double digit number present in it. 
-   This is done by checking the first element, and checking the maximum of the elements after it.
-   Then second element with the maximum of the elements after it and so on. '''
-  
-  sum = 0
-  for each_entry in entries:
-    overall_max_for_entry = 0 
-    for index in range(len(each_entry)-1): # checks till last second digit, as last digit need not be considered.
-      max_second_digit = 0
-      if int(each_entry[index]) < (overall_max_for_entry // 10): 
-          # checks if the first digit of the maximum number found is greater than all the other remaining elements. If it is not greater, then that element need not be considered, as the double digit number it generates will not be greater.
-          continue  
-      for other_index in range(index + 1, len(each_entry)): # checks all other elements after the element first selected. 
-        if int(each_entry[other_index]) > max_second_digit: # checks the maximum of the remaining elements for each iteration.
-          max_second_digit = int(each_entry[other_index])
-      two_digit_number = int(each_entry[index] + str(max_second_digit))    
-      if two_digit_number > overall_max_for_entry: # checks the maximum of all the elements in each entry
-        overall_max_for_entry = two_digit_number
-    sum += overall_max_for_entry
-  return sum
+  ''' Each entry is checked for the maximum double digit number in it.
+  First the highest digit is checked in all the elements except the last digit, as a double digit number is needed.
+  And then the highest digit in the elements after the first highest digit is checked.
+  Then sum of the all these maximum double digit numbers is done.'''
 
-start = time.time()
+  sum = 0  
+  for each_entry in entries:
+    max_first_digit = 0
+    max_second_digit = 0
+    index_of_max_digit = 0
+    for index in range(len(each_entry) - 1):
+        if int(each_entry[index]) > max_first_digit:
+          max_first_digit = int(each_entry[index])
+          index_of_max_digit = index
+    for index in range(index_of_max_digit + 1, len(each_entry)):
+        if int(each_entry[index]) > max_second_digit:
+           max_second_digit = int(each_entry[index])
+    max_two_digit_number = str(max_first_digit) + str(max_second_digit)
+    sum += int(max_two_digit_number)
+  return sum
+  
+start_time = time.time()
 print(list_of_input())
-stop = time.time()
-print(stop - start)
+end_time = time.time()
+print(f'Time taken to run the program is {end_time - start_time} seconds.')
