2c8c000cd2f559721a4e94d00c74bab7c47e65cb
naveenapyd <naveenapyd@gmail.com>
2025-12-06 16:28:53 +0530

    Improve day5 puzzle1 (iteration2)

--- day5/puzzle1.py
+++ day5/puzzle1.py
@@ -1,5 +1,7 @@
 # Day-5 Puzzle-1 : Cafeteria
 
+import time
+
 def list_of_input():
     file_path = 'day5/puzzle_input.txt'
     list_of_ranges = [] 
@@ -16,33 +18,30 @@ def list_of_input():
         list_of_ids = sections[1].split('\n')
     print(list_of_ranges)
     print(list_of_ids)
-    return get_count_of_overlapping_values(range_values(list_of_ranges), list_of_ids)
+    return get_count_of_overlapping_values(list_of_ranges, list_of_ids)
 
-def range_values(list_of_ranges):    
-    ''' Access the range of numbers present between first entry and second entry.
-    Similarly, between third and fourth, and fifth and sixth and so on.
-    Both first and second values should be inclusive. '''
-
-    ''' Storing all the range values in a list for the unique input is throwing a Memory Error.
-    However the code worked for the given test input. '''
-
-    all_range_values = []
-    for index in range(0, len(list_of_ranges), 2): # this will skip the index from 0 to 2 to 4 and so on.
-        starting_value_in_range = int(list_of_ranges[index]) # entries list has stored values as string. Hence convert to int.
-        ending_value_in_range = int(list_of_ranges[index + 1]) # index + 1 will give the odd value
-        current_range_values = list(range(starting_value_in_range, ending_value_in_range + 1)) # ending_value_in_range + 1 is done because for range function, the upper limit is exclusive.
-        all_range_values.extend(current_range_values)
-    print(all_range_values)
-    return all_range_values
-    
-def get_count_of_overlapping_values(all_range_values, list_of_ids):
-    ''' Each character in list_of_ids is checked if it is present in all_range_values.
+def get_count_of_overlapping_values(list_of_ranges, list_of_ids):
+    ''' Each character in list_of_ids is checked if it is present in list_of_ranges.
     If present then it is counted. '''
 
     count = 0
     for id in list_of_ids:
-        if int(id) in all_range_values:
-            count += 1
+        for index in range(0, len(list_of_ranges), 2):
+            # checking a pair of values in every loop - 0th element, 2nd element, 4th element, and so on, will be all the index values
+            lower_bound = int(list_of_ranges[index])
+            # 0th element, 2nd element, 4th element, and so on
+            upper_bound = int(list_of_ranges[index + 1])
+            # 1st element, 3rd element, 5th element and so on
+            if int(id) >= lower_bound and int(id) <= upper_bound:
+                # cheking if the number is within the range. Eg: if range is 3-5, then number should be greater than 3 and less than 5
+                count += 1
+                # when the number is found, this loop should end, so that it doesn't iterate for another range which might also contain the number
+                # Eg: number = 17, ranges = (16-20) and (12-18). The loop will iterate for both the ranges, and count will increase twice 
+                # In order to avoid this, break statement is used
+                break
     return count
 
+start_time = time.time()
 print(list_of_input())
+end_time = time.time()
+print(f'Time to run the code: {end_time - start_time} seconds.')
